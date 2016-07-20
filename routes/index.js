192ada64d611e4f762f3331f8c80ca52e3071f30
Methew Guda <methewguda@gmail.com>
2016-07-20 13:32:54 -0700

    Updating Data with HTTP verb PUT

--- routes/index.js
+++ routes/index.js
@@ -42,13 +42,27 @@ router.route('/books')
         res.status(201).send(book);
     });
 
-router.route('/books/:bookId')
+router.route('/:bookId')
 /* GET a single book by id */
     .get(function (req, res) {
         Book.findById(req.params.bookId, function (err, book) {
             if (err) {
                 res.status(500).send(err);
             } else {
+                res.json(book);
+            }
+        });
+    })
+    .put(function(req, res){
+        Book.findById(req.params.bookId, function (err, book) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                book.title = req.body.title;
+                book.author = req.body.author;
+                book.genre = req.body.genre;
+                book.read = req.body.read;
+                book.save();
                 res.json(book);
             }
         });
